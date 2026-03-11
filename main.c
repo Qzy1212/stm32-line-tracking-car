81d444bfe587e57a4b054aae93761946cbf52ddb
墙 <292100469+Qzy1212@users.noreply.github.com>
2026-03-11 07:00:00 +0000

    墙 2026.03.11 main：搭建主程序框架，集成传感器初始化

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "sensor.h"
+#include "pid.h"
+
+Sensor_Data_t   sensor_data;
+PID_Controller_t pid;
+
+void SystemClock_Config(void) { /* 由 STM32CubeMX 生成 */ }
+void MX_GPIO_Init(void)       { /* 由 STM32CubeMX 生成 */ }
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    Sensor_Init();
+
+    while (1)
+    {
+        Sensor_Read(&sensor_data);
+        /* TODO: PID 计算与电机控制 */
+    }
+}
